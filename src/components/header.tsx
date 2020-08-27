8f2cb939ab1d0809e49ac6a1b3b6fdff38a3361e
kevmca <kevinjmcandrew@gmail.com>
2020-08-27 18:54:54 +0100

    shadowing files added

---
+++ src/components/header.tsx
@@ -0,0 +1,39 @@
+/** @jsx jsx */
+import { jsx, useColorMode } from "theme-ui"
+import useMinimalBlogConfig from "../hooks/use-minimal-blog-config"
+import ColorModeToggle from "./colormode-toggle"
+import Navigation from "./navigation"
+import HeaderExternalLinks from "./header-external-links"
+
+const Header = () => {
+  const { navigation: nav } = useMinimalBlogConfig()
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const toggleColorMode = (e: any) => {
+    e.preventDefault()
+    setColorMode(isDark ? `light` : `dark`)
+  }
+
+  return (
+    <header sx={{ mb: [5, 6] }}>
+      <div
+        sx={{
+          boxSizing: `border-box`,
+          display: `flex`,
+          variant: `dividers.bottom`,
+          alignItems: `center`,
+          justifyContent: `space-between`,
+          mt: 3,
+          color: `secondary`,
+          a: { color: `secondary`, ":hover": { color: `heading` } },
+          flexFlow: `wrap`,
+        }}
+      >
+        <Navigation nav={nav} />
+        <HeaderExternalLinks />
+      </div>
+    </header>
+  )
+}
+
+export default Header
