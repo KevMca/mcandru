a9aa2ccf8dfb25af962a85fbceeb927825514b07
Michael McAndrew <mcandrmi@tcd.ie>
2020-09-10 10:47:33 +0100

    Refactor method of adding header image

--- src/components/project-header.tsx
+++ src/components/project-header.tsx
@@ -7,18 +7,18 @@ const ProjectHeader = (props) => {
 
   return (
     <div style={{display: "flex", flexDirection: "row", verticalAlign: `middle`}}>
-        <ProjectPicture name={`${props.name}`} slug={`${props.title.toLowerCase().split(" ").join("-")}`} />
-        <div sx={{ display: "flex", 
-                alignItems: "center", 
-                paddingLeft: "25px", 
-                paddingRight: "25px" 
-        }}>
-            <h2 sx={{ fontSize: [4, 5, 6], fontWeight: `bold`, color: `title` }}>
-                {props.title}
-            </h2>
-        </div>
-        <Status status={props.status} />
-        {props.children}
+      <ProjectPicture src={props.image} slug={`${props.title.toLowerCase().split(" ").join("-")}`} />
+      <div sx={{ display: "flex", 
+              alignItems: "center", 
+              paddingLeft: "25px", 
+              paddingRight: "25px" 
+      }}>
+          <h2 sx={{ fontSize: [4, 5, 6], fontWeight: `bold`, color: `title` }}>
+              {props.title}
+          </h2>
+      </div>
+      <Status status={props.status} />
+      {props.children}
     </div>
   )
 }
