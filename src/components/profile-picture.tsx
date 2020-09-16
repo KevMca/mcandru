8ef5b734feddde64e9507af785e9f621a2bc7b41
kevmca <kevinjmcandrew@gmail.com>
2020-09-16 17:16:16 +0100

    project picture updates

--- src/components/profile-picture.tsx
+++ src/components/profile-picture.tsx
@@ -6,7 +6,7 @@ const ProfilePicture = (props) => {
   return (
     <img 
         src={props.src}
-        sx={{ width: 150, objectFit: "cover", height: 150, borderRadius: 150/ 2, border: "1px solid grey" }}
+        sx={{ width: 150, objectFit: "cover", height: 150, borderRadius: 150/ 2, border: `0px` }}
     />
   )
 }
